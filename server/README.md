2125b3f641a83206148a355204610cfeb6cd538b
gtfx <rashkovsky@gmail.com>
2017-10-08 22:41:48 +0200

    Mock s (#2)
    * implemented docker-compose
    
    * Updated README with docker-compose
    
    * Some more info
    
    * Updated catch all

--- server/README.md
+++ server/README.md
@@ -2,12 +2,25 @@
 Provides a mock api to test instead of "real" puppetdb server. Required Docker to run
 
 ## Running
-To test with this server you need to have docker installed. First build the container
+To test with this server you need to have docker installed. You can use docker-comose or 
+
 ```
-docker built -t mock_server .
+docker-compose up
 ```
 
-After the build completes you can run the container
+Build docker image directly
 ```
-docker run -it -p 5000:5000 mock_server
+docker build -t '<image_name>' .
+```
+
+
+If you want to rebuild your container. Run the following command to rebuild without cache
+```
+docker-compose build --no-cache
+```
+
+You can also restart your container with your changes in code.
+
+```
+docker-compose up --build
 ```
--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 JSON_FILEPATH = 'json_files'
 
@@ -11,6 +11,9 @@ def get_full_path(filename):
 
 
 def get_json_file(filename):
+    if not filename.endswith('json'):
+        filename = '.'.join([filename, 'json'])
+
     full_path = get_full_path(filename)
 
     if os.path.isfile(full_path):
@@ -21,9 +24,14 @@ def get_json_file(filename):
 
     return {}
 
-@app.route('/reports')
-def reports():
-    reports_file = get_json_file('reports.json')    
 
-    return jsonify(reports_file)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if '/' in path:
+        path = path.replace('/', '_')
+
+    json_file = get_json_file(path)    
+
+    return jsonify(json_file)
 
